18be3387425ad5bf88b29c5a5a5561b00d4be4cf
Andrew <andrew@sullust.net>
2012-10-02 01:24:23 +0800

    clean up test output a bit

--- test/test.js
+++ test/test.js
@@ -57,29 +57,29 @@ function testOutput(module, useExecFile, filename, args, expectedOutput) {
 var prog = path.resolve('argv_as_json');
 
 child_process.execFile(prog, [], null, function(err,stdout,stderr) {
+	// run all the other tests after this check
 	if(err || stderr || !stdout)
 		throw Error("Could not run ./argv_as_json successfully. Maybe you need to compile args_as_json.c?");
-});
 
-// node's child_process (and argv_as_json) should behave!
-testOutput(child_process, true, prog, [], [prog]);
-testOutput(child_process, false, prog, [], [prog]);
-testOutput(child_process, true, prog, ['-a', '-b'], [prog, '-a', '-b']);
-testOutput(child_process, false, prog, ['-a', '-b'], [prog, '-a', '-b']);
-
-// supply argv0 explicity
-testOutput(child_process_with_argv0, true, prog, [prog], [prog]);
-testOutput(child_process_with_argv0, false, prog, [prog], [prog]);
-testOutput(child_process_with_argv0, true, prog, [prog, '-a', '-b'], [prog, '-a', '-b']);
-testOutput(child_process_with_argv0, false, prog, [prog, '-a', '-b'], [prog, '-a', '-b']);
-
-// supply a completely-made-up argv[0], should still work
-testOutput(child_process_with_argv0, true, prog, ['dne'], ['dne']);
-testOutput(child_process_with_argv0, false, prog, ['dne'], ['dne']);
-testOutput(child_process_with_argv0, true, prog, ['dne', '-a', '-b'], ['dne', '-a', '-b']);
-testOutput(child_process_with_argv0, false, prog, ['dne', '-a', '-b'], ['dne', '-a', '-b']);
-
-// don't accept empty argv[0]
+	// node's child_process (and argv_as_json) should behave!
+	testOutput(child_process, true, prog, [], [prog]);
+	testOutput(child_process, false, prog, [], [prog]);
+	testOutput(child_process, true, prog, ['-a', '-b'], [prog, '-a', '-b']);
+	testOutput(child_process, false, prog, ['-a', '-b'], [prog, '-a', '-b']);
+	
+	// supply argv0 explicity
+	testOutput(child_process_with_argv0, true, prog, [prog], [prog]);
+	testOutput(child_process_with_argv0, false, prog, [prog], [prog]);
+	testOutput(child_process_with_argv0, true, prog, [prog, '-a', '-b'], [prog, '-a', '-b']);
+	testOutput(child_process_with_argv0, false, prog, [prog, '-a', '-b'], [prog, '-a', '-b']);
+	
+	// supply a completely-made-up argv[0], should still work
+	testOutput(child_process_with_argv0, true, prog, ['dne'], ['dne']);
+	testOutput(child_process_with_argv0, false, prog, ['dne'], ['dne']);
+	testOutput(child_process_with_argv0, true, prog, ['dne', '-a', '-b'], ['dne', '-a', '-b']);
+	testOutput(child_process_with_argv0, false, prog, ['dne', '-a', '-b'], ['dne', '-a', '-b']);
+	
+	// don't accept empty argv[0]
 assert.throws(function(){
 	testOutput(child_process_with_argv0, true, prog, [], [prog]);
 });
@@ -95,10 +95,12 @@ assert.throws(function(){
 assert.throws(function(){
 	testOutput(child_process_with_argv0, false, prog, undefined, [prog]);
 });
+});
 
 		
 process.on('exit', function() {
 	assert.equal(testsRun, 12);
+	console.log('tested ok');
 });
 
 	
